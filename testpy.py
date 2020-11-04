2dd04d8c5555b53cd0af2087558163700f705f5e
maxpiras <massimopiras94@gmail.com>
2020-11-04 11:28:58 +0100

    Update testpy.py

--- testpy.py
+++ testpy.py
@@ -1,4 +1,4 @@
 path_to_data = "s3://zus-qa-s3/algoritmo1"
-with open('path_to_data + /test.txt', 'w') as file:
+with open(path_to_data + '/test.txt', 'w') as file:
   file.write('prova write')
   print('SUCCESS')
