ed918e3172368a7a774c58dee3985f6b303fedb5
maxpiras <massimopiras94@gmail.com>
2020-11-04 16:17:13 +0100

    Update testpy.py

--- testpy.py
+++ testpy.py
@@ -3,6 +3,7 @@ import datetime
 
 
 def main():
+    pip install s3fs
     tms = datetime.datetime.now().strftime("%H:%M:%S")
     print('********** STARTING **********')
     print(datetime.datetime.now().strftime("%D-%H:%M:%S"))
